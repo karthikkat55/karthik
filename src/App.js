027bcbffdc9e8b162190cc48b0e91e93f830eca1
karthik <karthik@email.com>
2023-01-11 17:55:18 +0530

    Adding files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NewFile from './NewFile';
+function App() {
+  return (
+    <div>
+    <div className="App">
+    <h1>Hey, I amn Kat</h1>
+    </div>
+    <div>
+      <NewFile/>
+    </div>
+    </div>)
+  
+}
+
+export default App;
---
+++ src/NewFile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './kat.css'
+function NewFile() {
+    return (
+     <div className="Bat">
+        <h>hello</h>
+        <p>hgdgdj</p>
+    </div>
+    );
+}
+export default NewFile;
